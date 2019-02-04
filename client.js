eb8f6a795be68962a2a9dba1d8269aa469619665
Spencer Faust <faus0059@umn.edu>
2019-02-03 18:58:36 -0600

    working on adjusting removed total

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ class Employee {
     }
 }
 
-let totalSalaries = 0;
+let totalSalaries = Number(0);
 let allEmployees = [];
 
 function isEmpty(str) {
@@ -23,19 +23,22 @@ function onReady() {
 };
 
 function submitButton() {
-    $('tbody').append('<tr><td>' + $('#firstName').val() + '</td><td>' + $('#lastName').val() + '</td><td>' + $('#idNumber').val() + '</td><td>' + $('#title').val() + '</td><td>' + $('#annualSalary').val() + '</td><td><button class="deleteButton">Delete</button></td></tr>');
+    $('tbody').append('<tr><td>' + $('#firstName').val() + '</td><td>' + $('#lastName').val() + '</td><td>' + $('#idNumber').val() + '</td><td>' + $('#title').val() + '</td><td class="salary">' + $('#annualSalary').val() + '</td><td><button class="deleteButton">Delete</button></td></tr>');
     totalSalaries += Number($('#annualSalary').val());
-    $('#totalMonthly').text('Total Monthly: ' + (Math.round(totalSalaries/12)));
+    $('#totalMonthly').text('Total Monthly: $' + (totalSalaries/12).toFixed(2));
     if((totalSalaries/12) > 20000) {
         $('#totalMonthly').css('background-color', 'red');
     } else {
         $('#totalMonthly').css('background-color', 'white');
     }
-    allEmployees.push(new Employee($('#firstName').val(), $('#lastName').val(), $('#idNumber').val(), $('#title').val(), $('#annualSalary').val()));
+    allEmployees.push(new Employee($('#firstName').val(), $('#lastName').val(), Number($('#idNumber').val()), $('#title').val(), Number($('#annualSalary').val())));
     $('.employeeInput').val('');
 }
 
 function deleteEmployeeButton() {
+    console.log(Number($(this).closest('tr.salary').text()));
     
+    // totalSalaries -= Number($(this.salary).text());
+    // $('#totalMonthly').text('Total Monthly: $' + Number((totalSalaries/12).toFixed(2)));
     $(this).closest('tr').remove();
 }
